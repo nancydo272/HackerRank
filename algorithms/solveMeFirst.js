2749c30ddd1c3ed011e3dfda72852cb50ea9eb88
Nancy Do <nancydo272@gmail.com>
2022-11-16 09:50:27 -0500

    first hackerrank algo

---
+++ algorithms/solveMeFirst.js
@@ -0,0 +1,9 @@
+// Complete the function solveMeFirst to compute the sum of two integers.
+//Exampple: a = 7, b =3 
+//Return 10
+
+function solveMeFirst(a, b) {
+    // Hint: Type return a+b below   
+    return a +b; 
+    }
+solveMeFirst(); 
